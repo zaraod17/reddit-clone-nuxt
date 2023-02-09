5071754db638f7d350641019833474da486e2183
marwal <mariusz.waldoch98@gmail.com>
2023-02-09 22:03:35 +0100

    Add main site blueprint

--- app.vue
+++ app.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <v-app id="inspire">
+    <!--  -->
+
+    <v-app-bar elevation="1">
+      <v-app-bar-nav-icon color="red" variant="text"
+        ><v-icon icon="mdi-reddit" size="large"></v-icon
+      ></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer> </v-navigation-drawer>
+    <v-main>
+      <!--  -->
+    </v-main>
+  </v-app>
 </template>
+
+<script lang="ts" setup>
+// const drawer = ref<boolean | null>(null);
+</script>
+
+<style scoped lang="scss">
+.v-application {
+  .v-app-bar-nav-icon {
+   
+  }
+}
+</style>
