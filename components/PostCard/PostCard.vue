a4d7b773d0987c5bfd04aed3eb2237c66b05182c
marwal <mariusz.waldoch98@gmail.com>
2023-03-12 20:45:17 +0100

    Add more styles & and export styles to separate file(PostCard), create new UserMenu component

--- components/PostCard/PostCard.vue
+++ components/PostCard/PostCard.vue
@@ -1,18 +1,33 @@
 <template>
   <v-sheet class="post-card" width="640">
     <div class="rating">
-      <v-icon>mdi-arrow-up-bold-outline</v-icon>
+      <v-icon class="like">mdi-arrow-up-bold-outline</v-icon>
       <h4>100</h4>
-      <v-icon>mdi-arrow-down-bold-outline</v-icon>
+      <v-icon class="dislike">mdi-arrow-down-bold-outline</v-icon>
     </div>
     <div class="post-content">
       <div class="post-title">
         <v-sheet style="display: flex; align-items: center">
           <v-avatar icon="mdi-account"></v-avatar>
-          <h6>Community</h6>
+          <v-menu
+            close-on-content-click
+            close-on-back
+            open-on-hover
+            location="bottom left"
+          >
+            <template #activator="{ props: menu }">
+              <h6 class="community-tag" v-bind="menu">Community</h6>
+            </template>
+            <tag-menu />
+          </v-menu>
           <span> . Posted by </span>
           <span> u/user123</span>
-          <span>x hours ago</span>
+          <v-tooltip location="top center">
+            <template #activator="{ props: tooltip }">
+              <span class="community-tag" v-bind="tooltip">x hours ago</span>
+            </template>
+            <p>some timestamp</p>
+          </v-tooltip>
         </v-sheet>
         <v-btn color="blue-darken-3" rounded="pill" size="small">Join</v-btn>
       </div>
@@ -45,79 +60,8 @@
   </v-sheet>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TagMenu from "../PopularTags/TagMenu.vue";
+</script>
 
-<style lang="scss" scoped>
-.post-card {
-  display: flex;
-  margin-top: 1rem;
-
-  .rating {
-    color: gray;
-    .v-icon {
-      cursor: pointer;
-    }
-
-    h4 {
-      color: black;
-    }
-  }
-  .post-content {
-    padding-left: 1rem;
-
-    .post-title {
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      margin-inline-end: 0.5rem;
-
-      .v-btn {
-        font-family: Noto Sans, Arial, sans-serif;
-        font-size: 12px;
-        font-weight: 700;
-        letter-spacing: unset;
-        line-height: 16px;
-        text-transform: unset;
-        box-shadow: none;
-      }
-      h6 {
-        font-size: 12px;
-        font-weight: 700;
-        line-height: 16px;
-        display: inline;
-        line-height: 20px;
-        text-decoration: none;
-        vertical-align: baseline;
-      }
-
-      span {
-        color: #787c7e;
-        font-size: 11px;
-        margin-inline: 2px;
-      }
-    }
-
-    .post-main {
-      font-weight: bold;
-      .v-sheet {
-        padding-inline: 0.2rem;
-      }
-      .v-img {
-        max-height: 500px;
-      }
-    }
-
-    .post-footer {
-      .v-btn {
-        font-size: 12px;
-        font-weight: 700;
-        line-height: 16px;
-        padding: 4px;
-        box-sizing: border-box;
-        text-transform: capitalize;
-        color: gray;
-      }
-    }
-  }
-}
-</style>
+<style src="./PostCardStyles.scss" lang="scss" scoped></style>
---
+++ components/UserMenu/UserMenu.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-sheet>
+    <div class="top-banner"></div>
+    <div class="avatar"></div>
+    <div class="nickname"></div>
+    <div class="user-time"></div>
+    <div class="create-btn"></div>
+    <div class="user-info"></div>
+    <div class="follow-btn"></div>
+  </v-sheet>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
